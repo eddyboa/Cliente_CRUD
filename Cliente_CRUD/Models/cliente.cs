552568bebed989eab0fa2b53a1d02783743d5ff1
Felipe Fausto <fellipe.fausto@hotmail.com>
2021-04-27 15:22:30 -0300

    Adicionar arquivos de projeto.

---
+++ Cliente_CRUD/Models/cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cliente_CRUD.Models
+{
+    public class cliente
+    {
+        [Key]
+        public int ID { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ Cliente_CRUD/Models/contexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Cliente_CRUD.Models
+{
+    public class contexto: DbContext
+    {
+        public DbSet<cliente> Clientes { get; set; }
+    }
+}
